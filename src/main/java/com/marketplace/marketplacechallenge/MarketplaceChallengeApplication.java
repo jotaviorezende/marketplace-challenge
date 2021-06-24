e7e501deada9914151ea414d2c5a2d5c1275ea25
Joao Otavio <jotaviorezende@gmail.com>
2021-06-24 19:10:10 -0300

    Estrutura inicial.

---
+++ src/main/java/com/marketplace/marketplacechallenge/MarketplaceChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.marketplace.marketplacechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplaceChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marketplace/marketplacechallenge/MarketplaceChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketplace.marketplacechallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
